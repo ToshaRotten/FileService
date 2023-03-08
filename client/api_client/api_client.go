9e01efc3254907ee2433730f710e9af7a0798c54
Tosha_Rotten <kozlovad@mer.ci.nsu.ru>
2023-03-08 20:17:47 +0700

    delete by name func

--- client/api_client/api_client.go
+++ client/api_client/api_client.go
@@ -1,6 +1,9 @@
 package api_client
 
 import (
+	"bytes"
+	"encoding/json"
+	"github.com/sirupsen/logrus"
 	"log"
 	"main/config"
 	"net/http"
@@ -10,13 +13,25 @@ var (
 	client = &http.Client{}
 )
 
+type Bar struct {
+	Hash [20]byte `json:"hash"`
+	Name string   `json:"name"`
+	Data string   `json:"data"`
+}
+
+type Foo struct {
+	Bar Bar `json:"bar"`
+}
+
 type APIClient struct {
 	Config *config.Config
+	Logger *logrus.Logger
 }
 
 func New(config *config.Config) *APIClient {
 	c := &APIClient{
 		Config: config,
+		Logger: logrus.New(),
 	}
 	return c
 }
@@ -24,14 +39,39 @@ func New(config *config.Config) *APIClient {
 func (c *APIClient) GetFileList() {
 	req, err := http.NewRequest("GET", "http://"+c.Config.Host+c.Config.Port+"/file/get/file_list", nil)
 	if err != nil {
-		log.Fatalln(err)
+		c.Logger.Error(err)
 	}
 	response, err := client.Do(req)
 	if err != nil {
-		log.Fatalln(err)
+		c.Logger.Error(err)
 	}
-	var result map[string]interface{}
-	//json.NewDecoder(response.Body).Decode(&result)
+	var result []Foo
+	dec := json.NewDecoder(response.Body)
+	err = dec.Decode(&result)
+	if err != nil {
+		c.Logger.Error(err)
+	}
 	log.Println(result)
 	defer response.Body.Close()
+}
+
+func (c *APIClient) DeleteFileByName(fileName string) {
+	hash := [20]byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+	foo := Foo{
+		Bar: Bar{
+			Hash: hash,
+			Name: "",
+			Data: "",
+		},
+	}
+	body, _ := json.Marshal(foo)
+	req, err := http.NewRequest("DELETE", "http://"+c.Config.Host+c.Config.Port+"/file/delete", bytes.NewBuffer(body))
+	if err != nil {
+		c.Logger.Error(err)
+	}
+	response, err := client.Do(req)
+	if err != nil {
+		c.Logger.Error(err)
+	}
+	c.Logger.Info(response.Status)
 }
